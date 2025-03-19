07a372211b88bef3d1b7523a4e1c6517118fb5a0
Arskiz <129842259+Arskiz@users.noreply.github.com>
2025-03-19 12:03:14 +0100

    Update install-dependencies.bat

--- install-dependencies.bat
+++ install-dependencies.bat
@@ -13,7 +13,7 @@ if errorlevel 1 (
 
 echo Checking if requirements.txt exists...
 if not exist requirements.txt (
-    echo Error: requirements.txt not found! Make sure you're in the correct directory.
+    echo Error: requirements.txt not found! Make sure you're in the correct directory and that you haven't deleted the file.
     pause
     exit /b 1
 )
@@ -29,4 +29,26 @@ if errorlevel 1 (
 
 echo --------------------------------------------------------
 echo Dependencies installed successfully!
+echo --------------------------------------------------------
+echo Creating essential folders...
+mkdir media
+if errorlevel 1 (
+    echo Error: Error creating media folder
+    pause
+    exit /b 1
+)
+mkdir media\uploads
+if errorlevel 1 (
+    echo Error: Error creating media\uploads folder
+    pause
+    exit /b 1
+)
+mkdir media\extracted_nets
+if errorlevel 1 (
+    echo Error: Error creating media\extracted_nets folder
+    pause
+    exit /b 1
+)
+echo --------------------------------------------------------
+echo Folders created successfully!
 pause
