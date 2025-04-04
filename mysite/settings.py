99f640d399358f63128f35e100506006ef56c90e
Arskiz <129842259+Arskiz@users.noreply.github.com>
2025-04-04 11:12:06 +0200

    Final Update 1.5

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,16 +42,6 @@ INSTALLED_APPS = [
     "cpn", # Our application - Petrinet visualization
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
 ROOT_URLCONF = 'mysite.urls'
 
 MIDDLEWARE = [
